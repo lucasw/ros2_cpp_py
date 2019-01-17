09f9df798b7727bddea2046f6b2fb28184e2e8ac
Lucas Walter <wsacul@gmail.com>
2019-01-17 13:38:41 -0800

    Including another launch file, and overlaying the argument- the top level one is used.  What about contradictory types? #2

--- launch/example.launch.py
+++ launch/example.launch.py
@@ -21,22 +21,31 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    arg1 = launch.substitutions.LaunchConfiguration('test')
-    context = LaunchContext()
-
-    print(context)
+    # arg1 = launch.substitutions.LaunchConfiguration('node_name')
+    # context = LaunchContext()
+    # print(context)
     # print("{}".format(arg1.perform(context)))
     # print("{}, {}, {}".format(arg1.describe, arg1.perform, arg1.variable_name))
     # print("{}".format(dir(arg1)))
 
     return LaunchDescription([
         launch.actions.DeclareLaunchArgument(
-            'test',
-            default_value='default_value',
+            'image1',
+            default_value='test.png',
+            description='image arg'),
+        launch_ros.actions.Node(
+            package='image_publisher', node_executable='image_publisher', output='screen',
+            node_name='image1',
+            arguments=[launch.substitutions.LaunchConfiguration('image1')],
+            # remappings=[('image_raw', 'image_in')],
+            ),
+        launch.actions.DeclareLaunchArgument(
+            'test_arg',
+            default_value='foo',
             description='test'),
         launch_ros.actions.Node(
             package='ros2_cpp_py', node_executable='cpp_test', output='screen',
-            node_name=launch.substitutions.LaunchConfiguration('test'),
+            node_name=launch.substitutions.LaunchConfiguration('test_arg'),
             parameters=[{"test": 11}],
             ),
     ])
